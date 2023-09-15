03881715ddbe16f4c1c3a4da796ae6a6aebec3c0
Maurizio Branca <maurizio.branca@gmail.com>
2023-09-15 10:02:10 +0200

    Improve the !iphones command
    Handle no phone response and other minor cases.

--- dolores/commands.py
+++ dolores/commands.py
@@ -4,24 +4,39 @@ import discord
 import httpx
 from discord.ext import commands
 
+# The discord.ext.commands framework docs are available at:
+# https://discordpy.readthedocs.io/en/stable/ext/commands/commands.html
+#
+# The Discord docs for message formatting:
+# Markdown Text 101 (Chat Formatting: Bold, Italic, Underline) 
+# https://support.discord.com/hc/en-us/articles/210298617-Markdown-Text-101-Chat-Formatting-Bold-Italic-Underline
+
 
 @commands.command()
 async def ping(ctx):
 	await ctx.send('pong')
 
 @commands.command()
-async def iphones(ctx, name: str = 'iPhone 13 Pro Max'):
+async def iphones(ctx, name: str = '13 Pro Max'):
+	"""This command will send a list of refurbished iPhones from the Apple Store."""
 	async with httpx.AsyncClient() as session:
 		r = await session.get('https://raw.githubusercontent.com/zmoog/refurbished-history/main/stores/it/iphones.json')
-		if r.status_code == 200:
-			all_iphones = json.loads(r.text)
+		if r.status_code != 200:
+			await ctx.send(f'Something went wrong (http status f{r.status_code}).')
+			print(r.text)
+			return
 
-			filtered_iphones = list(filter(lambda x: name in x['name'], all_iphones))
+		all_iphones = json.loads(r.text)
+		filtered_iphones = list(filter(lambda x: name in x['name'], all_iphones))
 
-			msg = ''
-			for iphone in filtered_iphones[:5]:
-				msg += f'- [{iphone["name"]}]({iphone["url"]}) — {iphone["price"]}\n'
+		if len(filtered_iphones) == 0:
+			await ctx.send('No iPhones found.')
+			return
 
-			print(msg)
+		msg = f'Here are the latest iPhones {name}:\n'
+		for iphone in filtered_iphones[:10]:
+			msg += f'- [{iphone["name"]}]({iphone["url"]}) — {iphone["price"]} (-{iphone["savings_price"]})\n'
 
-			await ctx.send(msg)
+		print(msg)
+
+		await ctx.send(msg)
